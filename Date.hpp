6729db90f43c5dd6bd0dab37516345cd1c70acb6
Ivan-Kuttuev <47279931+Ivan-Kuttuev@users.noreply.github.com>
2019-04-20 21:29:38 +0300

    Add files via upload

--- Date.hpp
+++ Date.hpp
@@ -52,6 +52,14 @@ namespace ext
 	*/
 	unsigned int countJND(Date date);
 
+	//cin >> day;	
+	//	cin >> month;
+	//	cin >> year;
+	//
+	//	int a = (14 - month) / 12;
+	//	int y = year + 4800 - a;
+	//	int m = month + 12 * a - 3;
+	//	cout << day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045 << endl;
 	/*
 	Рассчитывает количество дней между двумя датами.
 	При реализвации используйте CountJND
