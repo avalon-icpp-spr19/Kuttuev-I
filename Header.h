c39993d4c2ed28f8d4b8cc3692dd983697d5d710
Ivan-Kuttuev <47279931+Ivan-Kuttuev@users.noreply.github.com>
2019-04-20 20:20:06 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <iostream>
+void helloWorld(const char * line); // функция ;(char * line) - Мы хотим вынести  line ;остальное в Header.cpp ,header.h нужен для обращения из Функции.cpp
+
